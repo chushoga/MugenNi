39c35bd8d34f4f12e98d8966d2b75424c8fcd58a
chushoga <chushoga@gmail.com>
2018-05-21 13:54:18 +0900

    In middle of setting up charing power
    - charges too fast
    - the gravity is weird after jumping.

--- Assets/_PROTOTYPE/Scripts/PlayerController.cs
+++ Assets/_PROTOTYPE/Scripts/PlayerController.cs
@@ -1,26 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
-	private readonly Vector3 LAUNCH_VELOCITY = new Vector3(20f, 60f, 0f);
-	private readonly Vector3 INITIAL_POSITION = Vector3.zero;
+	private Vector3 LAUNCH_VELOCITY = new Vector3(20f, 60f, 0f);
+	private Vector3 INITIAL_POSITION = Vector3.zero;
 	private readonly Vector3 GRAVITY = new Vector3(0f, -240f, 0f);
-	private const float DELAY_UNTIL_LAUNCH = 4f;
 	private int NUM_DOTS_TO_SHOW = 15;
 	private float DOT_TIME_STEP = 0.02f;
 
-	private bool launched = false;
-	private float timeUntilLaunch = DELAY_UNTIL_LAUNCH;
 	private Rigidbody rigidBody;
 
 	public GameObject trajectoryDotPrefab;
+	public GameObject trajectoryContainer;
 
 	// JUMP PERAMETERS
 	public float jumpForce = 0.0f;
 	public float jumpForceMax = 100.0f;
+	public bool isCharging = false;
 
 	private void Awake()
 	{
@@ -30,49 +30,45 @@ public class PlayerController : MonoBehaviour
 
 	private void Start()
 	{
-		Time.fixedDeltaTime = 0.002f;
-		for (int i = 0; i < NUM_DOTS_TO_SHOW; i++)
-		{
-			GameObject trajectoryDot = Instantiate(trajectoryDotPrefab);
-			trajectoryDot.transform.position = CalculatePosition(DOT_TIME_STEP * i );
-		}
+		// Set up the trajectory container
+		trajectoryContainer = new GameObject();
+		trajectoryContainer.name = "trajectoryContainer";
 
+		// set the fixed timestep to a faster calc time
+		Time.fixedDeltaTime = 0.002f;
+
+		DrawTrajectory();
 	}
 
-	private void Update()
+	private void FixedUpdate()
 	{
-		timeUntilLaunch -= Time.deltaTime;
 
-		if (!launched && timeUntilLaunch <= 0)
-		{
-			Launch();
-		}
-
-		if(Input.GetKeyDown(KeyCode.Space)){
-			Launch();
-		}
-
-		if(Input.touchCount > 0 || Input.GetMouseButton(0)) {			
+		if(Input.touchCount > 0 || Input.GetMouseButton(0)) {		
 			IncreaseJumpForce();
+			isCharging = true;
 			Debug.Log(jumpForce);
 		} else {
 			if(jumpForce != 0.0f) {
 				jumpForce = 0.0f;
 			}
+			isCharging = false; // reset the charging if no longer charging.
+		}
+
+		// Check the jump power charging state.
+		if(!isCharging) {
+			trajectoryContainer.SetActive(false); // Hide the trajectory container if jumping
+			Jump(); // jump
+		} else {
+			trajectoryContainer.SetActive(true); // Show the trajectory container if jumping
+			DrawTrajectory(); // if still charging keep drawing the trajectory
 		}
 
 	}
 
-	private void Launch()
-	{
-		
-		rigidBody.velocity = LAUNCH_VELOCITY;
-		launched = true;
-		//Debug.Log(LAUNCH_VELOCITY);
-	}
-
 	private Vector2 CalculatePosition(float elapsedTime)
 	{
+		LAUNCH_VELOCITY = gameObject.transform.forward * jumpForce;
+		INITIAL_POSITION = gameObject.transform.position;
 		return GRAVITY * elapsedTime * elapsedTime * 0.5f + LAUNCH_VELOCITY * elapsedTime + INITIAL_POSITION;
 	}
 
@@ -80,11 +76,54 @@ public class PlayerController : MonoBehaviour
 	public void IncreaseJumpForce(){
 		if(jumpForce != jumpForceMax) {
 			jumpForce += 1;
+			DrawTrajectory();
+		}
+	}
+
+	// Draw the trajectory prediction
+	public void DrawTrajectory(){
+		// Populate the trajectory master if it is empty.
+		if(trajectoryContainer.transform.childCount == 0) {
+			for(int i = 0; i < NUM_DOTS_TO_SHOW; i++) {
+
+				// Set up for alpha.
+				float alpha = 1 - ((float)i / NUM_DOTS_TO_SHOW);
+
+				// Instatntiate the trajectory dot and set its parent
+				GameObject trajectoryDot = Instantiate(trajectoryDotPrefab);
+				trajectoryDot.transform.parent = trajectoryContainer.transform;
+
+				// Set the alpha of the trajectory dots to gradually fade out.
+				Material col = trajectoryDot.GetComponent<Renderer>().material;
+				trajectoryDot.GetComponent<Renderer>().material.color = new Color(col.color.r, col.color.g, col.color.b, alpha);
+
+				// Set the trajectroy dot positions
+				trajectoryDot.transform.position = CalculatePosition(DOT_TIME_STEP * i);
+
+			}
+
+		} else {
+			// Transform the postion of the dots here if they are already instantiated
+			for(int i = 0; i < trajectoryContainer.transform.childCount; i++) {
+
+				float alpha = 1 - ((float)i / NUM_DOTS_TO_SHOW);
+
+				GameObject trajectoryDot = trajectoryContainer.transform.GetChild(i).gameObject;
+				trajectoryDot.transform.position = CalculatePosition(DOT_TIME_STEP * i);
+
+			}
 		}
 	}
 
 	public void Jump(){
-		
+
+		// set fixed update interverval to a higher rate for more accurate results.
+		Time.fixedDeltaTime = 0.002f;
+
+		// Set the launch velocity and launch the player.
+		LAUNCH_VELOCITY = gameObject.transform.position * jumpForce;
+		rigidBody.velocity = LAUNCH_VELOCITY;
+
 		Debug.Log("JUMP ME" + jumpForce);
 
 	}
