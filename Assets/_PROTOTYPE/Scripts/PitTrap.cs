cdd374c4d1d436685b68a9fab5cdd12182e1303b
chushoga <chushoga@gmail.com>
2018-06-13 13:09:38 +0900

    Tested with a hing trap that drops when the player walks on it.

---
+++ Assets/_PROTOTYPE/Scripts/PitTrap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitTrap : MonoBehaviour {
+	
+	public GameObject hing;
+	private HingeJoint hj;
+
+	// Use this for initialization
+	void Start () {
+		hj = hing.GetComponent<HingeJoint>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag == "Player") {
+
+			JointLimits limits = hj.limits;
+			limits.max = 90;
+			hj.limits = limits;
+
+		}
+	}
+}
