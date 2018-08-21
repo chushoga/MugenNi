45d849d60cba2e30f4fd0657b946682d4d7982ce
chushoga <chushoga@gmail.com>
2018-08-21 17:36:48 +0900

    started adding a slide menu for the main info
    This will work better I think for preformance and general handleing of the scenes for the main menu.

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public Animator contentPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToggleMenu()
+    {
+        bool isHidden = contentPanel.GetBool("isHidden");
+        contentPanel.SetBool("isHidden", !isHidden);
+    }
+}
