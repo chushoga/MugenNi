47b6a4f70ed44316a6a40a82d341040f309d8226
chushoga <chushoga@gmail.com>
2018-09-03 18:42:48 +0900

    minor update

---
+++ Assets/Scripts/GameData/Master.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Master : MonoBehaviour {
+
+    // Include on all scenes.
+    // Persistant game information loaded from the save file.
+
+    // Coin count
+    public static int COIN_COUNT = 0;
+    public static int TOTAL_JUMPS = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+}
