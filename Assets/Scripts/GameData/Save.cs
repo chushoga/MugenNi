9f6d34b961632e85713035d1e6d2651a504f356a
chushoga <chushoga@gmail.com>
2018-09-05 18:07:51 +0900

    update

--- Assets/Scripts/GameData/Save.cs
+++ Assets/Scripts/GameData/Save.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 
 [System.Serializable]
 public class Save {
-
-    public List<int> levelProgress = new List<int>(); // level progression(stars) 1 = normal clear. 2,3,4 = 1,2,3 stars found and collected in each level.
+        
     public List<float> levelClearTime = new List<float>(); // level clear time
     public List<int> levelJumpCount = new List<int>(); // level jump count
     public List<int> unlockedSkins = new List<int>(); // unlocked skins
     
     public int coins; // The total coins collected. //TODO add use for the coins other than just count.
+
+    public string[] levelProgress = new string[64]; // level progression(stars) 1 = normal clear. 2,3,4 = 1,2,3 stars found and collected in each level.
 
     public int lifetimeJumps;
     public int lifetimeDeaths;
