88f28e2b29dbf2dcf1a2c1512a7c936c5c6db8da
chushoga <chushoga@gmail.com>
2018-09-12 16:14:10 +0900

    LoadingSuccess
    Successfully added Load and save game in either JSON or data serialization!!! YATTA

---
+++ Assets/Scripts/GameData/Game.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+
+    public Text console;
+
+    public int coins;
+
+    // TEST ADD COINS
+    public void AddCoins()
+    {
+        coins = coins + 1;
+        console.text = "COINS: " + coins;
+    }
+    
+    // Create a save game object
+    private Save CreateSaveGameObject()
+    {
+        Save save = new Save();
+
+        save.coins = coins;
+
+        return save;
+    }
+
+    public void SaveGame()
+    {
+        // 1 Create a Save instance with all the data for the current session saved into it.
+        Save save = CreateSaveGameObject();
+
+        // 2 Create a BinaryFormatter and a FileStream by passing a path for the Save instnce
+        // to be saved to. It serializes the data into bytes and writes it to disk and closes
+        // the FileStream. ** can use any extention that you want, does not need to be .save
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, save);
+        file.Close();
+
+        print("gameSaved");
+
+
+        console.text = "CONIS: " + coins;
+
+    }
+
+    public void LoadGame()
+    {
+        // 1
+        // checks if there is a save game and if there is then it will open and set the variables in the save file.
+        if(File.Exists(Application.persistentDataPath + "/gamesave.save"))
+        {
+            // 2
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
+            Save save = (Save)bf.Deserialize(file);
+            file.Close();
+
+            // 4
+            coins = save.coins;
+            console.text = "COINS: " + coins;
+
+            print("Loaded");
+        } else
+        {
+            print("No gave saved-----!");
+        }
+
+    }
+
+    public void SaveAsJSON()
+    {
+        Save save = CreateSaveGameObject();
+        string json = JsonUtility.ToJson(save);
+
+        string path = Application.persistentDataPath + "/saveGame.json";
+        File.WriteAllText(path, json);
+
+        //Save saver = JsonUtility.FromJson<Save>(json);
+
+        Debug.Log("Saveing as Json" + json);
+    }
+
+    public void LoadAsJSON()
+    {
+        // checks if there is a save game and if there is then it will open and set the variables in the save file.
+        if (File.Exists(Application.persistentDataPath + "/saveGame.json"))
+        {
+            // 2
+            
+            string path = Application.persistentDataPath + "/saveGame.json";
+            string json = File.ReadAllText(path);
+            
+            Save save = (Save)JsonUtility.FromJson(json, typeof(Save));
+            
+            // 4
+            coins = save.coins;
+            console.text = "COINS: " + coins;
+
+            print("Loaded" + json);
+        }
+        else
+        {
+            print("No gave saved-----!");
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameData/GameSettings.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSettings : MonoBehaviour {
+
+    [SerializeField]
+    private Toggle toggle;
+
+    [SerializeField]
+    private AudioSource bgMusic;
+
+    public void Awake()
+    {
+
+        // If there is no key with bgMusic create it and set default to on.
+        // else read the bgMusic variable and toggle depending on the int 0(off) or 1(on).
+        // 1
+        if (!PlayerPrefs.HasKey("bgMusic"))
+        {
+            PlayerPrefs.SetInt("bgMusic", 1);
+            toggle.isOn = true;
+            bgMusic.enabled = true;
+            PlayerPrefs.Save();
+        }
+
+        // 2
+        else
+        {
+            if(PlayerPrefs.GetInt("bgMusic") == 0)
+            {
+                bgMusic.enabled = false;
+                toggle.isOn = false;
+
+            } else
+            {
+                bgMusic.enabled = true;
+                toggle.isOn = true;
+            }
+        }
+
+    }
+
+    // Toggle the music
+    public void ToggleMusic()
+    {
+        if (toggle.isOn)
+        {
+            PlayerPrefs.SetInt("bgMusic", 1);
+            bgMusic.enabled = true;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("bgMusic", 0);
+            bgMusic.enabled = false;
+        }
+
+        PlayerPrefs.Save();
+    }
+
+    
+}
