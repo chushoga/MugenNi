99515dc23e78eb05c59cfc711e196cd1e94d8dc1
chushoga <chushoga@gmail.com>
2018-10-23 18:46:05 +0900

    up

--- Assets/StarPickup.cs
+++ Assets/StarPickup.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class StarPickup : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int starPosition;
+    
+    void OnTriggerEnter(Collider col)
+    {
+
+        // Check if it is the player or not
+        if (col.gameObject.tag == "Player")
+        {
+            GlobalControl.Instance.LoadedData.worldData[GlobalControl.Instance.currentWorld].levelData[GlobalControl.Instance.currentLevel].stars[starPosition] = 1;                  
+            Destroy(gameObject);
+        }
+    }
 }
