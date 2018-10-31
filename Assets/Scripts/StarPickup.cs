7046245ab6b65a41a03be63b5b2bb46303d88af2
chushoga <chushoga@gmail.com>
2018-10-31 17:20:42 +0900

    updated a bit

--- Assets/Scripts/StarPickup.cs
+++ Assets/Scripts/StarPickup.cs
@@ -7,47 +7,48 @@ public class StarPickup : MonoBehaviour {
     public int starPosition;
     public bool isActive;
 
+    private Color starColor;
+
     void Start()
     {
-        
+
         int currentWorldId = GlobalControl.Instance.currentWorld;
         int currentLevelId = GlobalControl.Instance.currentLevel;
 
         int counter = 0; // counter for the foreach
         int starCount = 0; // total stars at true
-        
-        
+
         print(GlobalControl.Instance.LoadedData.worldData[0].levelData[0].stars.Length + " [TEST-----------------]");
-        
-        if(GlobalControl.Instance.LoadedData.worldData[currentWorldId].levelData[currentLevelId].stars[starPosition] == 1)
+
+        // set the star color for each starMesh child.        
+        foreach(Transform child in gameObject.transform)
         {
-            print("ACTIVE +++++++++++++++++++++++++++++++++");
-            isActive = true;
-        } else
-        {
-            print("INACTIVE xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-            isActive = false;
+            if(child.name == "starMesh")
+            {
+                print("test");
+                starColor = child.GetComponent<Renderer>().material.color;
+                starColor.a = 0.5f;
+            }
         }
 
-        /*
-             foreach (int stars in GlobalControl.Instance.LoadedData.worldData[currentWorldId].levelData[counter].stars)
-             {
-                 if (stars == 1)
-                 {
-                     starCount++;
-                 }
-             }
+        // if the star flag is 1 then change the color
+        if (GlobalControl.Instance.LoadedData.worldData[currentWorldId].levelData[currentLevelId].stars[starPosition] == 0)
+        {
+            // Set the opacity of the object
+            gameObject.GetComponentInChildren<Renderer>().material.color = starColor;
+            isActive = false; 
+        } else
+        {   
+            isActive = true;
+        }
 
-             print(starCount);
-         */
     }
 
     void OnTriggerEnter(Collider col)
     {
         // Check if it is the player or not
         if (col.gameObject.tag == "Player")
-        {           
-
+        {  
             GlobalControl.Instance.LoadedData.worldData[GlobalControl.Instance.currentWorld].levelData[GlobalControl.Instance.currentLevel].stars[1] = 1;                  
             Destroy(gameObject);
         }
