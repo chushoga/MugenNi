76160576485adcd6139b1470d7f378b5e45a6aeb
chushoga <chushoga@gmail.com>
2019-06-21 13:10:10 +0900

    added spawn point
    TODO: make the collider bigger maybe

---
+++ Assets/Scripts/ParticleSystem/AutoStartParticleSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoStartParticleSystem : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // gameObject.GetComponent<ParticleSystem>().Play(true);        
+    }
+
+}
